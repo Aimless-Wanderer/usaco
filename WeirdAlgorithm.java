f61d3f24add8efdfed376f710ccc77e4eb1f0973
Aimless-Wanderer <kevin.chang7677@gmail.com>
2023-11-11 16:22:39 -0800

    quick edit

--- WeirdAlgorithm.java
+++ WeirdAlgorithm.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class WeirdAlgorithm {
     public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+	    long n = sc.nextLong();
 		
             System.out.print(n + " ");
 
