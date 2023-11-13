33d0375136b718e2e08b0ada742e4afb83b8ffe5
Aimless-Wanderer <kevin.chang7677@gmail.com>
2023-11-12 23:04:00 -0800

    day 2

--- fencepainting.java
+++ fencepainting.java
@@ -32,27 +32,6 @@ public class fencepainting {
     public static int findLength(int a1, int b1, int a2, int  b2) {
         int length = 0;
 
-        //Here I tried to brute force it but there are too many holes
-        // if(a1 > b2 || b1 < a2) {
-        //     length = b1+b2-a1-a2;
-        // }else if (a1 == a2 && b1 == b2){
-        //     length = b2-a2;
-        // } else if(a2 == b1) {
-        //     length = b1-a2;
-        // } else if(a1 == b2){
-        //     length = b2-a1;
-        // } else if(a1 > a2 && b1 < b2){
-        //     length = b2-a2;
-        // } else if(a1 < a2 && b1 > b2){
-        //     length = b1-a1;
-        // } else if (b2 > a1){
-        //     length = b1 - a2;
-        // } else if(b1 > a2){
-        //     length = b2 - a1;
-        // }
-
-        //I stole one glance at the solution, and I got inspiration in literally making the fence an array
-
         int[] fence = new int[100];
         for(int i = a1; i < b1; i++) {
             fence[i] = 1;
