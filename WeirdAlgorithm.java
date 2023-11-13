102f492a97a152cfc19e5d3e4bd8999b49d65d70
Aimless-Wanderer <kevin.chang7677@gmail.com>
2023-11-12 22:55:52 -0800

    day1

--- WeirdAlgorithm.java
+++ WeirdAlgorithm.java
@@ -2,18 +2,19 @@ import java.util.Scanner;
 
 public class WeirdAlgorithm {
     public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-	    long n = sc.nextLong();
-		
-            System.out.print(n + " ");
+		try (Scanner sc = new Scanner(System.in)) {
+            long n = sc.nextLong();
+            
+                System.out.print(n + " ");
 
-        while(n != 1) {
-            if (n % 2 == 0) {
-                n /= 2;
-            } else {
-                n = 3 * n + 1;
+            while(n != 1) {
+                if (n % 2 == 0) {
+                    n /= 2;
+                } else {
+                    n = 3 * n + 1;
+                }
+                System.out.print(n + " ");
             }
-            System.out.print(n + " ");
         }
     }
 }
