08ff7ff09a0c3c04e3e1945679ad23ee39dca912
Aimless-Wanderer <kevin.chang7677@gmail.com>
2024-01-04 20:12:45 -0800

    first python!

--- Week 5/Day 28/SleepingInClass.java
+++ Week 5/Day 28/SleepingInClass.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class SleepingInClass {
---
+++ Week 5/Day 29/CowGymnastic.py
@@ -0,0 +1,20 @@
+with open("gymnastics.in") as read:
+	num_sessions, num_cows = [int(i) for i in read.readline().split()]
+	sessions = []
+	for i in range(num_sessions):
+		sesh = [int(j) for j in read.readline().split()]
+		sessions.append(sesh)
+  
+consistent_pairs = 0
+
+for i in range (1, num_cows+1):
+    for j in range(1, num_cows+1):
+        greater = True
+        if i != j:
+            for k in range(len(sessions)):
+                if sessions[k].index(i) < sessions[k].index(j):
+                    greater = False
+        if greater:
+        	consistent_pairs+=1
+            
+print(int(consistent_pairs/2), file=open("gymnastics.out", "w"))
